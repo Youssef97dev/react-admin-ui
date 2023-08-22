782a2e94f52ac3f22d65c24f10d99a3f89430277
Youssef97dev <youssefmoussafi97@gmail.com>
2023-08-22 05:33:37 -1100

    updating for deployement

--- src/components/dataTable/DataTable.tsx
+++ src/components/dataTable/DataTable.tsx
@@ -15,11 +15,6 @@ type Props = {
 };
 
 const DataTable = (props: Props) => {
-  const handleDelete = (id: number) => {
-    //delete the item
-    // mutation.mutate(id)
-  };
-
   const actionColumn: GridColDef = {
     field: "action",
     headerName: "Action",
@@ -30,7 +25,7 @@ const DataTable = (props: Props) => {
           <Link to={`/${props.slug}/${params.row.id}`}>
             <img src="/view.svg" alt="" />
           </Link>
-          <div className="delete" onClick={() => handleDelete(params.row.id)}>
+          <div className="delete">
             <img src="/delete.svg" alt="" />
           </div>
         </div>
--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -10,8 +10,8 @@ import { menu } from "../../data";
 const Menu = () => {
   return (
     <div className="menu">
-      {menu.map((item) => (
-        <div className="item" key={item.key}>
+      {menu.map((item, index) => (
+        <div className="item" key={index}>
           <span className="title">{item.title}</span>
           {item.listItems.map((listItem) => (
             <Link to={listItem.url} className="listItem" key={listItem.id}>
